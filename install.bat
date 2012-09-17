005f925c6976d42e22b08d697e42a75dacbe7f66
Andrey Bogdanov <the-dark-angel@yandex.ru>
2012-09-17 22:52:33 +0600

    2012.09.17-22:52

--- install.bat
+++ install.bat
@@ -2,8 +2,9 @@
 echo Installing...
 mkdir "%programfiles%\TeX Creator\plugins"
 mkdir "%programfiles%\TeX Creator\macros"
-copy .\build\release\tex-creator.exe "%programfiles%\TeX Creator"
-copy "%programfiles%\BeQt\lib\beqtcore0.dll" "%programfiles%\TeX Creator"
-copy "%programfiles%\BeQt\lib\beqtgui0.dll" "%programfiles%\TeX Creator"
-copy "%programfiles%\BeQt\lib\beqtnetwork0.dll" "%programfiles%\TeX Creator"
+del "%programfiles%\TeX Creator\tex-creator.exe"
+del "%programfiles%\TeX Creator\beqt*"
+copy ".\build\release\tex-creator.exe" "%programfiles%\TeX Creator"
+copy "%programfiles%\BeQt\lib\beqtcore*" "%programfiles%\TeX Creator"
+copy "%programfiles%\BeQt\lib\beqtgui*" "%programfiles%\TeX Creator"
 echo Installation finished.
--- install.sh
+++ install.sh
@@ -5,12 +5,17 @@ mkdir -p /usr/lib/tex-creator/other
 mkdir -p /usr/lib/tex-creator/beqt
 mkdir -p /usr/lib/tex-creator/plugins
 mkdir -p /usr/share/tex-creator/macros
+rm /usr/lib/tex-creator/tex-creator
+rm /usr/lib/tex-creator/beqt/libbeqtcore.*
+rm /usr/lib/tex-creator/beqt/libbeqtgui.*
+rm /usr/bin/tex-creator.sh
+rm /usr/share/applications/tex-creator.desktop
+rm /usr/share/pixmaps/tex-creator.png
 cp ./build/tex-creator /usr/lib/tex-creator
+cp -P /usr/lib/libbeqtcore.* /usr/lib/tex-creator/beqt
+cp -P /usr/lib/libbeqtgui.* /usr/lib/tex-creator/beqt
 cp ./install/tex-creator.sh /usr/bin
 cp ./install/tex-creator.desktop /usr/share/applications
 cp ./logo.png /usr/share/pixmaps/tex-creator.png
-cp /usr/lib/libbeqtcore.so.0 /usr/lib/tex-creator/beqt
-cp /usr/lib/libbeqtgui.so.0 /usr/lib/tex-creator/beqt
-cp /usr/lib/libbeqtnetwork.so.0 /usr/lib/tex-creator/beqt
 echo "Installation finished."
 exit 0
