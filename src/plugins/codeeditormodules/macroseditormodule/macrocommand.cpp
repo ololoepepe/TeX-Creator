fab372d138e133b556a2699b3df86a4d0172da45
Andrey Bogdanov <ololoepepe@gmail.com>
2014-03-25 21:12:31 +0600

    Fix escape sequences in macro system

--- src/plugins/codeeditormodules/macroseditormodule/macrocommand.cpp
+++ src/plugins/codeeditormodules/macroseditormodule/macrocommand.cpp
@@ -56,13 +56,15 @@ static QStringList getArgs(const QString &text, int &i, char opbr, char clbr, in
     QString s;
     while (i < text.length() && (text.at(i) != nopbr ||  text.at(i - 1) == '\\' || depth))
     {
-        if (text.at(i) == clbr && (text.at(i - 1) != '\\' || text.at(i - 2) == '\\'))
+        if (text.at(i) == clbr && text.at(i - 1) != '\\')
+        //if (text.at(i) == clbr && (text.at(i - 1) != '\\' || text.at(i - 2) == '\\'))
         {
             --depth;
             if (depth)
                 s += clbr;
         }
-        else if (text.at(i) == opbr && (text.at(i - 1) != '\\' || text.at(i - 2) == '\\'))
+        else if (text.at(i) == opbr && text.at(i - 1) != '\\')
+        //else if (text.at(i) == opbr && (text.at(i - 1) != '\\' || text.at(i - 2) == '\\'))
         {
             if (depth)
                 s += opbr;
